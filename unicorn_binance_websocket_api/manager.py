0f1dbd4a79090ae071d36eb05d82a94e9b20ca18
free <219649240+rava0sh@users.noreply.github.com>
2025-06-24 19:00:14 +0200

    dev version

--- unicorn_binance_websocket_api/manager.py
+++ unicorn_binance_websocket_api/manager.py
@@ -75,7 +75,7 @@ import orjson
 import websockets
 
 __app_name__: str = "unicorn-binance-websocket-api"
-__version__: str = "2.10.2"
+__version__: str = "2.10.2.dev"
 __logger__: logging.getLogger = logging.getLogger("unicorn_binance_websocket_api")
 
 logger = __logger__
